9be31dd5ac1a7b5f629cc5900beb7734206a919b
Guilherme Matheus Corrêa <guilherme.matheus.correa@gmail.com>
2020-03-21 23:25:12 -0300

    feat: Global Style

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 24px;
-  color: ${(props) => (props.error ? '#ff0000' : '#7159c1')};
-
-  small {
-    font-size: 14px;
-    color: #333;
-  }
+  color: #fff;
 `;
