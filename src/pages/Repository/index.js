ac955ed453419af5331fb7e47c7a055f76d62681
Guilherme Matheus Corrêa <guilherme.matheus.correa@gmail.com>
2020-03-22 21:13:42 -0300

    feat: Load repository data from GitHub api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../services/api';
 
 // import { Container } from './styles';
 
 export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(async () => {
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repositoryResponse, issuesResponse] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    setRepository(repositoryResponse.data);
+    setIssues(issuesResponse.data);
+  }, []);
+
+  return <h1>Repository</h1>;
 }
